fb7d5934d3d96cb5a588bef775256f57a2a629f9
Vincent Marklynn <vincent.marklynn@gmail.com>
2021-07-08 11:56:07 -0700

    Completed Exercise 1

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,14 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+//Exercise 1
+const firstP = document.createElement('p');
+firstP.textContent = "Hey I'm Red!";
+firstP.setAttribute('style', 'color:red');
+
+container.appendChild(firstP);
+
+
+
   
