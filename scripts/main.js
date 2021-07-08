7f76ff1b84d2425cae550d05d6e47941c2085d5d
Vincent Marklynn <vincent.marklynn@gmail.com>
2021-07-08 12:25:57 -0700

    Completed exercise 3

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,17 @@ newH3.setAttribute('style','color: blue');
 
 container.appendChild(newH3);
 
+//Exercise 3 
+const newDiv = document.createElement('div');
+newDiv.setAttribute('style', 'border: 1px; border-style: solid');
+ 
+const newH1 = document.createElement('h1');
+newH1.textContent = "I'm in a div!";
 
+const secondP = document.createElement('p');
+secondP.textContent = "I'm also in a div!"
 
+newDiv.append(newH1);
+newDiv.append(secondP);
+container.append(newDiv);
   
